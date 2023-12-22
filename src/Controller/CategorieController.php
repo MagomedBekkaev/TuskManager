7a32842d5f52b67fbf8a12463eed23e48207b449
Magomed Bekkaev <bekkaev.magomed@gmail.com>
2023-12-22 14:41:50 +0100

    reset

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -13,27 +13,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CategorieController extends AbstractController
 {
     #[Route('/categorie', name: 'app_categorie')]
-public function index(CategorieRepository $categorieRepository, TacheRepository $tacheRepository): Response
-{
-    $categories = $categorieRepository->findAll();
-    $taches = $tacheRepository->findBy([], ["dateCreation" => "ASC"]);
+    public function index(CategorieRepository $categorieRepository, TacheRepository $tacheRepository): Response
+    {
+        $categories = $categorieRepository->findAll();
+        $taches = $tacheRepository->findBy([], ["dateCreation" => "ASC"]);
 
-    // Organisez les tâches par catégorie
-    $tachesByCategory = [];
-    foreach ($categories as $categorie) {
-        $tachesByCategory[$categorie->getId()] = []; // Initialisez le tableau même si la catégorie n'a pas de tâches
+        // Organisez les tâches par catégorie
+        $tachesByCategory = [];
+        foreach ($categories as $categorie) {
+            $tachesByCategory[$categorie->getId()] = []; // Initialisez le tableau même si la catégorie n'a pas de tâches
+        }
+
+        foreach ($taches as $tache) {
+            $categoryId = $tache->getCategorie()->getId();
+            $tachesByCategory[$categoryId][] = $tache;
+        }
+
+        return $this->render('categorie/index.html.twig', [
+            'categories' => $categories,
+            'tachesByCategory' => $tachesByCategory,
+        ]);
     }
-
-    foreach ($taches as $tache) {
-        $categoryId = $tache->getCategorie()->getId();
-        $tachesByCategory[$categoryId][] = $tache;
-    }
-
-    return $this->render('categorie/index.html.twig', [
-        'categories' => $categories,
-        'tachesByCategory' => $tachesByCategory,
-    ]);
-}
-
-
 }
