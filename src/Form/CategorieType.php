324ea3e9f79e905b82868789b42ea1773d228aee
Magomed Bekkaev <bekkaev.magomed@gmail.com>
2023-12-30 16:26:07 +0100

    cleaning code

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -50,11 +50,4 @@ class CategorieType extends AbstractType
             'data_class' => Categorie::class,
         ]);
     }
-
-    // public function configureOptions(OptionsResolver $resolver): void
-    // {
-    //     $resolver->setDefaults([
-    //         'data_class' => Categorie::class,
-    //     ]);
-    // }
 }
